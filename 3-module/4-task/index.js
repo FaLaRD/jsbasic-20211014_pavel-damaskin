71b624c3d483f41df1b0b4fcd7674e4ac9329a21
Damaskin Pavel <falard.damaskin@gmail.com>
2021-10-23 15:47:56 +0300

    3.4 task is ready

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,17 @@
 function showSalary(users, age) {
-  // ваш код...
+  if (!Array.isArray(users)) {
+    throw new SyntaxError('Argument must be an Array');
+  }
+
+  let res = '';
+
+  const filteredUsers = users.filter(user => age >= user.age);
+
+  filteredUsers.forEach((user, ix) => {
+    const lineBreak = (filteredUsers.length - 1 === ix) ? '' : '\n';
+
+    res += `${user.name}, ${user.balance}${lineBreak}`;
+  });
+
+  return res;
 }
