bc16b2133abd58867b584f5720edf4cc5907c97a
Damaskin Pavel <falard.damaskin@gmail.com>
2021-10-23 15:48:19 +0300

    3.5 task is ready

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  const res = {};
+
+  const filteredNumbers = str.split(' ').filter(item => isFinite(item));
+
+  const min = Math.min(...filteredNumbers);
+  const max = Math.max(...filteredNumbers);
+
+  res.min = min;
+  res.max = max;
+
+  return res;
 }
