82aaf8b7b799b2b79ef6a7f33439e34a7d64e93f
Damaskin Pavel <falard.damaskin@gmail.com>
2021-10-26 20:56:15 +0300

    4.1 task is ready

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,17 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  if (typeof friends !== 'object' && friends === null) {
+    throw new SyntaxError('First argument must be an Object type');
+  }
+
+  const ul = document.createElement('ul');
+
+  for (let friend in friends) {
+    const li = document.createElement('li');
+    const { firstName, lastName } = friends[friend];
+
+    li.append(document.createTextNode(`${firstName} ${lastName}`));
+    ul.append(li);
+  }
+
+  return ul;
 }
