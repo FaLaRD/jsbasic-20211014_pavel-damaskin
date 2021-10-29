3171eeae18e1a6621b4e37c6588fcdcc1bef44e3
Damaskin Pavel <falard.damaskin@gmail.com>
2021-10-29 11:26:59 +0200

    5.1 task is ready

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,13 @@
 function hideSelf() {
-  // ваш код...
+  const btns = document.querySelectorAll('.hide-self-button');
+
+  if (!btns.length) {
+    return;
+  }
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      e.currentTarget.hidden = true;
+    });
+  });
 }
