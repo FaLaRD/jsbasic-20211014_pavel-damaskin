09b001c583f33095951c1ffa5365f7d701581875
Damaskin Pavel <falard.damaskin@gmail.com>
2021-11-04 19:19:07 +0200

    6.2 task is ready

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,46 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this._elem = this.createNode(product);
+    this.add2cart(product.id);
+  }
+
+  get elem() {
+    return this._elem;
+  }
+
+  createNode({ name, price, image }) {
+    return createElement(`
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${image}" class="card__image" alt="product">
+          <span class="card__price">€${price.toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${name}</div>
+          <button type="button" class="card__button">
+             <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `);
+  }
+
+  add2cart(id) {
+    document.addEventListener('click', ({ target, currentTarget }) => {
+      const add2cartBtn = target.closest('.card__button');
+
+      if (!add2cartBtn) {
+        return;
+      }
+
+      const evt = new CustomEvent('product-add', {
+        detail: id,
+        bubbles: true,
+      });
+
+      this._elem.dispatchEvent(evt);
+    });
   }
 }
